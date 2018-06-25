cc1d67820b851790dbf58038873f7836e6293dbf
Khizra <khiz@pwr.local>
2018-06-24 19:25:14 -0700

    added err handling in promiseAll

--- prod/server/index.js
+++ prod/server/index.js
@@ -97,8 +97,12 @@ app.post('/sqrt', function(req, res){
 app.get('/stats', function(req, res) {
 
   var stats = []
-  Promise.all([helper.aggregator('factorial'), helper.aggregator('basic'), helper.aggregator('power'), helper.aggregator('log'), helper.aggregator('sqrt')]).then(function(stats) {
+  Promise.all([helper.aggregator('factorial'), helper.aggregator('basic'), helper.aggregator('power'), helper.aggregator('log'), helper.aggregator('sqrt')])
+  .then(function(stats) {
         res.send(stats)
+    })
+  .catch(function(reason) {
+      console.log(reason)
     })
   })
 
