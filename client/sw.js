7db35c6b9d6bf43affd9da79ac537977c8177e06
Khizra <khiz@pwr.local>
2018-06-26 00:17:39 -0700

    added all resources that should be cached

--- client/sw.js
+++ client/sw.js
@@ -3,6 +3,22 @@ self.addEventListener('install', function(e) {
    caches.open('pwa-add-store').then(function(cache) {
      return cache.addAll([
        '/',
+       '/components',
+       '/components/add.js',
+       '/components/app.js',
+       '/components/funcItem.js',
+       '/components/slides.js',
+       '/components/statsItem.js',
+       '/services',
+       '/services/items.js',
+       '/angular',
+       '/angular/angular.js',
+       '/templates',
+       '/templates/add.html',
+       '/templates/app.html',
+       '/templates/func-item.html',
+       '/templates/slides.html',
+       '/templates/stats-item.html',
        '/index.js',
        '/index.html',
        '/styles.css'
