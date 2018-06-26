b4d945d3ecb961e808cb363545d12116321511fe
Khizra <khiz@pwr.local>
2018-06-25 21:28:55 -0700

    service worker and client side storage

--- client/index.js
+++ client/index.js
@@ -1,1 +1,17 @@
+
+
 angular.module('app', []);
+
+window.onload = function() {
+
+// angular.module('app', []);
+  if('serviceWorker' in navigator) {
+    navigator.serviceWorker
+             .register('/sw.js')
+             .then(function() {
+              console.log('Service Worker Registered'); })
+             .catch(function(err) {
+                console.log(err)
+             })
+  }
+}
