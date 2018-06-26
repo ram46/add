882a274d1c141112067efbed9aa1064eb2101e51
Khizra <khiz@pwr.local>
2018-06-25 22:52:04 -0700

    changed store name

--- client/index.js
+++ client/index.js
@@ -3,11 +3,13 @@
 angular.module('app', []);
 
 window.onload = function() {
+
+// angular.module('app', []);
   if('serviceWorker' in navigator) {
     navigator.serviceWorker
              .register('/sw.js')
              .then(function() {
-                console.log('Service Worker Registered'); })
+              console.log('Service Worker Registered'); })
              .catch(function(err) {
                 console.log(err)
              })
--- client/sw.js
+++ client/sw.js
@@ -1,6 +1,6 @@
 self.addEventListener('install', function(e) {
  e.waitUntil(
-   caches.open('video-store').then(function(cache) {
+   caches.open('pwa-add-store').then(function(cache) {
      return cache.addAll([
        '/sw.js',
        '/styles.css',
